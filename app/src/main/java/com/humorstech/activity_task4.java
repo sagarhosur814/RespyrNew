c29ce0b9665e51423c398d73f3eaff6fc8a0ad69
Uphar Gaur <uphargaur@gmail.com>
2023-10-21 02:45:00 +0530

    completed task 4

---
+++ app/src/main/java/com/humorstech/activity_task4.java
@@ -0,0 +1,14 @@
+package com.humorstech;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.humorstech.respyr.R;
+
+public class activity_task4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task4);
+    }
+}
